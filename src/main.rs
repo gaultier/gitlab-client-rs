4bd546fd2994d324bf17c6b2ca791ed787611477
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-04-27 12:54:08 +0200

    Add project_id to future

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let token = std::env::var("GITLAB_TOKEN")?;
     let project_ids: Vec<u64> = vec![138, 125, 156, 889, 594];
     let client = reqwest::Client::new();
+    let projects_count = project_ids.len();
 
     let bodies = stream::iter(project_ids)
         .map(|project_id| {
@@ -18,18 +19,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     ))
                     .header("PRIVATE-TOKEN", token)
                     .send()
-                    .await?
+                    .await
+                    .map_err(|err| (project_id, err))?
                     .text()
                     .await
+                    .map(|body| (project_id, body))
+                    .map_err(|err| (project_id, err))
             }
         })
-        .buffer_unordered(5);
+        .buffer_unordered(projects_count);
 
     bodies
         .for_each(|body| async {
             match body {
-                Ok(b) => println!("Ok: {}", b.len()),
-                Err(e) => eprintln!("Error: {}", e),
+                Ok((project_id, b)) => println!("[{}] Ok: {}", project_id, b.len()),
+                Err((project_id, e)) => eprintln!("[{}] Error: {}", project_id, e),
             }
         })
         .await;
